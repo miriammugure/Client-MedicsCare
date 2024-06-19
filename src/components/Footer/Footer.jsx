b38edf9acdd7223c0eb81aec2411360e9ffe09ed
miriam <miriammugure610@gmail.com>
2024-06-19 14:52:53 +0300

    add the footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Date from "../../utils/Date";
+import Icons from "../Icons/Icons";
 import "./Footer.css";
 function Footer() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="footerContainer">
+        <div className="footerNav">
+          <div className="FooterLinks">
+            <Icons
+              twitterUrl={"twitter.com"}
+              fbUrl={"facebook.com"}
+              instaUrl={"instagram.com"}
+            />
+          </div>
+          <div className="Footertext">
+            <p> &copy;2024 all rights reserved. miriam mugure wachira </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
