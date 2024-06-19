4483dc0476c074a622566b71a58b6d0afc2816cb
miriam <miriammugure610@gmail.com>
2024-06-19 20:26:37 +0300

    add the about section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "./Hero/Hero";
+import About from "./About/About";
 function Home() {
   return (
     <div>
       <Hero />
+      <About />
     </div>
   );
 }
