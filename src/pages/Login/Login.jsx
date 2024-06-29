3b0b85f11662d4df8d12e5b0d567488180dec918
miriam <miriammugure610@gmail.com>
2024-06-29 10:48:47 +0300

    add the login section

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,33 +1,32 @@
-// import React from 'react'
-// import '../Register/Register.css'
-// import Title from '../../components/Title/Title'
-// import regiterimg from '../../assets/register.jpg'
-// function Login() {
-//   return (
-//     <div>
-//         <div className="registerContainer">
-//         <div className="formContainer">
-//         <Title
-//       title="register here"/>
-//         <form action="">
+import React from "react";
+import "../Register/Register.css";
+import Title from "../../components/Title/Title";
+import regiterimg from "../../assets/register.jpg";
+function Login() {
+  return (
+    <div>
+      <div className="registerContainer">
+        <div className="formContainer">
+          <Title title="register here" />
+          <form action="">
+            <label htmlFor="email">email:</label>
+            <input type="text" placeholder="johndoe@example.com" />
 
-//         <label htmlFor="email">email:</label>
-//         <input type="text" placeholder='johndoe@example.com'/>
+            <label htmlFor="password">password:</label>
+            <input type="password" />
 
-//         <label htmlFor="password">password:</label>
-//         <input type="password" />
+            <button type="submit">submit</button>
+          </form>
+        </div>
+        <div className="registerIMg">
+          <img
+            src={regiterimg}
+            alt="an animated image of a doctor and a patient"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
 
-//        <button type='submit'>submit</button>
-
-//       </form>
-//         </div>
-//         <div className="registerIMg">
-//             <img src={regiterimg} alt="an animated image of a doctor and a patient" />
-//         </div>
-//       </div>
-
-//     </div>
-//   )
-// }
-
-// export default Login
+export default Login;
