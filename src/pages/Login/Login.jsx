630f1d4f3394d52b5ea80ba0858a84c53bf8db98
miriam <miriammugure610@gmail.com>
2024-07-02 14:35:09 +0300

    add formik to the register and login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,18 +2,63 @@ import React from "react";
 import "../Register/Register.css";
 import Title from "../../components/Title/Title";
 import regiterimg from "../../assets/register.jpg";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 function Login() {
+  const validationSchema = Yup.object({
+    email: Yup.string("email should be a string")
+      .email("email must be valid")
+      .required("email is required"),
+
+    passWord: Yup.string("password must be a string")
+      .required("password is required")
+      .min(8, "password must be atleast 8 characters long"),
+  });
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      passWord: "",
+    },
+    onSubmit: (formState) => {
+      console.log(formState);
+    },
+    validationSchema: validationSchema,
+  });
+
   return (
     <div>
       <div className="registerContainer">
         <div className="formContainer">
           <Title title="log in here" />
-          <form action="" className="registerform">
+          <form
+            action=""
+            className="registerform"
+            onSubmit={formik.handleSubmit}
+          >
             <label htmlFor="email">email:</label>
-            <input type="text" placeholder="johndoe@example.com" />
+            <input
+              type="text"
+              placeholder="johndoe@example.com"
+              name="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.phoneNumber && formik.errors.phoneNumber && (
+              <p>{formik.errors.phoneNumber}</p>
+            )}
 
             <label htmlFor="password">password:</label>
-            <input type="password" />
+            <input
+              type="password"
+              name="passWord"
+              value={formik.values.passWord}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.passWord && formik.errors.passWord && (
+              <p>{formik.errors.passWord}</p>
+            )}
 
             <button type="submit">submit</button>
           </form>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,27 +2,107 @@ import React from "react";
 import "./Register.css";
 import Title from "../../components/Title/Title";
 import regiterimg from "../../assets/register.jpg";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 function Register() {
+  const validationSchema = Yup.object({
+    firstName: Yup.string("name must be a string").required(
+      "first name is required",
+    ),
+    lastName: Yup.string("name must string").required("last name is required"),
+    email: Yup.string("email should be a string")
+      .email("email must be valid")
+      .required("email is required"),
+
+    passWord: Yup.string("password must be a string")
+      .required("password is required")
+      .min(8, "password must be atleast 8 characters long"),
+  });
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phoneNumber: "",
+      passWord: "",
+    },
+    onSubmit: (formState) => {
+      console.log(formState);
+    },
+    validationSchema: validationSchema,
+  });
   return (
     <div>
       <div className="registerContainer">
         <div className="formContainer">
           <Title title="register here" />
-          <form action="" className="registerform">
+          <form
+            action=""
+            className="registerform"
+            onSubmit={formik.handleSubmit}
+          >
             <label htmlFor="firstName">first name:</label>
-            <input type="text" placeholder="john" />
-
+            <input
+              type="text"
+              placeholder="john"
+              name="firstName"
+              value={formik.values.firstName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.firstName && formik.errors.firstName && (
+              <p>{formik.errors.firstName}</p>
+            )}
             <label htmlFor="lastName">last name:</label>
-            <input type="text" placeholder="doe" />
+            <input
+              type="text"
+              placeholder="doe"
+              name="lastName"
+              value={formik.values.lastName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.lastName && formik.errors.lastName && (
+              <p>{formik.errors.lastName}</p>
+            )}
 
             <label htmlFor="email">email:</label>
-            <input type="text" placeholder="johndoe@example.com" />
+            <input
+              type="text"
+              placeholder="johndoe@example.com"
+              name="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.email && formik.errors.email && (
+              <p>{formik.errors.email}</p>
+            )}
 
             <label htmlFor="phoneNumber">phone number:</label>
-            <input type="text" placeholder="011045678" />
+            <input
+              type="text"
+              placeholder="011045678"
+              name="phoneNumber"
+              value={formik.values.phoneNumber}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.phoneNumber && formik.errors.phoneNumber && (
+              <p>{formik.errors.phoneNumber}</p>
+            )}
 
             <label htmlFor="password">password:</label>
-            <input type="password" />
+            <input
+              type="password"
+              name="passWord"
+              value={formik.values.passWord}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.passWord && formik.errors.passWord && (
+              <p>{formik.errors.passWord}</p>
+            )}
 
             <div className="selectdropdown">
               <p>who are you</p>
