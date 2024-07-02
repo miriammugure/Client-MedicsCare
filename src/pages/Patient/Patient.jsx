2e72171bfc746ce6bf810e03ac90bce3137d3518
miriam <miriammugure610@gmail.com>
2024-07-02 06:57:08 +0300

    add the booking component

--- src/pages/Patient/Patient.jsx
+++ src/pages/Patient/Patient.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Bmi from "../../components/BMI/Bmi";
+import Booking from "../../components/Booking/Booking";
 import "./Patient.css";
 function Patient() {
   return (
     <div>
       <div className="patientContainer">
         <Bmi />
+        <Booking />
       </div>
     </div>
   );
